7db7437de7e7ffbfc82d42ad673f51493cd091f5
Bohdan_Novickyi <61537968+psy-ger@users.noreply.github.com>
2021-09-12 03:13:17 +0300

    first update file

---
+++ lab2-course4/lab2_file1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lab2_file2;
+
+namespace la2_file1
+{
+    class lab2_file1_class
+    {
+        lab2_file2_class class_call = new lab2_file2_class();
+
+    }
+}
---
+++ lab2-course4/lab2_file2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_file2
+{
+    class lab2_file2_class
+    {
+
+    }
+}
