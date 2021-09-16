1e4f253df78b60d70822d1fb64590034fa8b3b62
Bohdan_Novickyi <61537968+psy-ger@users.noreply.github.com>
2021-09-16 05:46:55 +0300

    full lab2

--- lab2-course4/lab2_file1.cs
+++ lab2-course4/lab2_file1.cs
@@ -9,7 +9,15 @@ namespace la2_file1
 {
     class lab2_file1_class
     {
-        lab2_file2_class class_call = new lab2_file2_class();
+        static void Main()
+        {
+
+            lab2_file2_class class_call = new lab2_file2_class();
+            Console.WriteLine(class_call.Enter_N());
+            Console.WriteLine(class_call.Enter_A());
+            Console.WriteLine(class_call.CheakN());
+            Console.WriteLine(class_call.Formula());
+        }
 
     }
 }
--- lab2-course4/lab2_file2.cs
+++ lab2-course4/lab2_file2.cs
@@ -8,6 +8,44 @@ namespace lab2_file2
 {
     class lab2_file2_class
     {
+        public int n;
+        public int a;
+        public int sum_formula;
+        public string Enter_N()
+        {
+            n =Convert.ToInt32(Console.ReadLine());
+            return "";
+        }
+        public string Enter_A()
+        {
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n");
+            return "";
+        }
+        public string CheckA()
+        { 
+            
+            return "";
+        }
+        public string CheakN()
+        {
+            if (n % 1 == 0)
+            {
+                Console.WriteLine("Целое"); 
+            }
+            return "";
+        }
+        public string Formula()
+        {
+            sum_formula = a * (a + 1);
 
+            for (int i = 0; i < (n-2); i++)
+            {
+                sum_formula *= (a + n - 1);
+                Console.WriteLine(sum_formula);          
+            }
+            return "";
+        }
+        
     }
 }
